df425854e1e3a37508fa7497f34fedf960683414
yerang <buuuuung@naver.com>
2021-04-15 14:33:03 +0900

    PS

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -15,62 +15,44 @@
 //int n,m,i,j,sum=0,cnt=0,a, b,ran;
 
 using namespace std;
+//답은 ans, 함수는 solve, 부울은 flag, start/endXY대신에 start만 받고 사이즈 받기 방법
+int n, m, cnt;
 
-int arr[2187][2187];
-int i, j, n,cnt[3];
-
-void recursion(int startX, int endX, int startY, int endY){
+void recursion(vector<int>& vv){
     //base case
-    if(startX==endX&&startY==endY) {
-        cnt[arr[startY][startX]+1]++;
+    if(vv.size()==n){
+        for(auto p:vv) cout<<p<<' ';
+        cout<<'\n';
         return;
     }
-    
-    //recursion
-   bool isChange=false;
-   int tmp=arr[startY][startX];
-   for(int t=startY;t<=endY;t++){
-       for(int p=startX;p<=endX;p++){
-           if(arr[t][p]!=tmp){
-               isChange=true;
-               goto label;
-           }
-       }
-   }
-    label:
-   //전체 통일이면
-   if(!isChange){
-       cnt[tmp+1]++;
-   }
-   //통일 아니면 9구간으로 나누기
-   else{
-       int varX = (endX-startX+1)/3;
-       int varY = (endY-startY+1)/3;
-       for(int t=0;t<3;t++){
-           for(int p=0;p<3;p++){
-               recursion(startX+p*varX, startX+(p+1)*varX-1,
-                         startY+t*varY, startY+(t+1)*varY-1);
-           }
-       }
-   }
-   return;
+    //recursive case
+    for(int i=1;i<=m;i++){
+        if(!vv.empty()){
+            //cout<<"i : "<<i<<" / m : "<< m <<'\n';
+            //원소가 존재하면
+            bool isIn = false;
+            for(int j=0;j<vv.size();j++){
+                if(vv[j]==i) isIn=true;
+            }
+            if(isIn){
+                //cout<<"컨티뉴!\n";
+                continue;
+            }
+        }
+        //원소가 존재하지 않으면
+        vv.push_back(i);
+        recursion(vv);
+        vv.pop_back();
+    }
+    return;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    cin>>n;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cin>>arr[i][j];
-        }
-    }
+    cin>>m>>n;
+    vector<int> v;
+    recursion(v);
     
-    //재귀 진입
-    recursion(0,n-1,0,n-1);
-    
-    for(auto p:cnt)cout<<p<<'\n';
 }
-/*
- 
- */
+
