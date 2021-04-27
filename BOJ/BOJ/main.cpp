064f936bb91f53324581131ad04cfdd36a7d9295
yerang <buuuuung@naver.com>
2021-04-27 15:56:41 +0900

    Solving problems samsung and so on

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -7,7 +7,7 @@
 #include <stack>
 #include <set>
 #include <map>
-//#include<bits/stdc++.h>
+#include <tuple>
 
 //#define INT_MAX 2147483647
 //bool compare(int a, int b) {return a>b;}
@@ -15,56 +15,149 @@
 
 using namespace std;
 
-int n, m, k;
+int n, ans=0;
 //상하좌우 순서
+bool visit[21][21][21];
 int axisX[4]={0,0,-1,1};
 int axisY[4]={-1,1,0,0};
-bool visit[2][1001][1001]; //부수기 전/후 방문여부
-int mapp[1001][1001];
-queue<pair<pair<int, int>,pair<int, bool>>> q;
-int main() {
-    //ios::sync_with_stdio(false);
-    //cin.tie(NULL); cout.tie(NULL);
-    
-    scanf("%d %d",&n,&m);
-    //맵 입력
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            scanf("%1d",&mapp[i][j]);
-        }
-    }
-    q.push(make_pair(make_pair(0,0),make_pair(1,false)));
+int mapp[21][21];
+vector<pair<int, int>> pv;
 
-    visit[0][0][0]=true;
-    while(!q.empty()){
-        int x, y, d;
-        bool c;
-        x=q.front().first.second;
-        y=q.front().first.first;
-        d=q.front().second.first;
-        c=q.front().second.second;
-        q.pop();
-        if(x==m-1&&y==n-1){
-            cout<<d<<'\n';
-            goto label;
-        }
+void move(int t);
+void getMax(){
+    
+}
+
+void solve(int cnt){
+    if(cnt>=5){
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                ans=max(ans, mapp[i][j]);
+        return;
+    }
+    //네 방향 이동 : 상하좌우
+    for(int i=0;i<4;i++){
+        int temp[20][20];
+        bool isComp[20][20]={false,};
+        for(int ii=0;ii<n;ii++)
+            for(int jj=0;jj<n;jj++)
+                temp[ii][jj]=mapp[ii][jj];
         
-        for(int i=0;i<4;i++){
-            int dy=y+axisY[i];
-            int dx=x+axisX[i];
-            if(dy>=0&&dy<n&&dx>=0&&dx<m){
-                if(mapp[dy][dx]==1&&!c&&!visit[c][dy][dx]&&mapp[y][x]!=1){
-                    q.push(make_pair(make_pair(dy,dx),make_pair(d+1,true)));
-                    visit[c][dy][dx]=true;
-                }
-                else if(mapp[dy][dx]==0&&!visit[c][dy][dx]){
-                    q.push(make_pair(make_pair(dy,dx),make_pair(d+1,c)));
-                    visit[c][dy][dx]=true;
+        //상향 -> y값 작은애들부터 옮기기
+        if(i==0){
+            for(int ii=2;ii<n;ii++){
+                for(int jj=0;jj<n;jj++){
+                    int dy=ii, dx=jj;
+                    while(1){ //계속 옮기기
+                        int &before=temp[dy][dx];
+                        dy+=axisY[i];
+                        dx+=axisX[i];
+                        //0이 아니면(값이 있으면)
+                        if(temp[dy][dx]!=0){
+                            //0이 아닌데 값이 같으면
+                            if(temp[dy][dx]==before){
+                                //이전에 합쳐진 놈이 아니면...
+                                if(!isComp[dy][dx]){
+                                    //합치고 true할당
+                                    temp[dy][dx]*=2;
+                                    before=0;
+                                    isComp[dy][dx]=true;
+                                }
+                            }
+                            //값이 다르면
+                            if(temp[dy][dx]!=before){
+                                break;
+                            }
+                        }
+                        //0이면 그냥 옮겨야지.
+                        temp[dy][dx]=before;
+                        before=0;
+                    }
                 }
             }
         }
     }
-    cout<<-1<<'\n';
-    label:
+}
+/*
+void move(int t){
+    //방향에 대한 Axis 연쇄적으로 더하기. pv[0]부터 순서대로.
+    //움직일 항에 다른 값이 있는데,
+    // -같으면 움직일 항에 현재 항 더하고, 현재 항은 0으로 바꾸기
+    // -다르면 멈추기.
+    for(int i=0;i<(int)pv.size();i++){
+        int dy=pv[i].first;
+        int dx=pv[i].second;
+        
+        //계속 반복
+        while(1){
+            cout<<"move! "<<dy<<','<<dx<<'\n';
+            dy+=axisY[t];
+            dx+=axisX[t];
+            //맵 벗어나면 멈추기
+            if(dy<0||dy>=n||dx<0||dx>=n) break;
+            //다음 항에 이미 값이 있으면
+            int &now=mapp[dy-axisY[t]][dx-axisX[t]];
+            if(mapp[dy][dx]!=0){
+                //값이 다르면 멈추기
+                if(now!=mapp[dy][dx]){
+                    break;
+                }
+                //값이 같으면 합치기
+                else if(now==mapp[dy][dx]){
+                    mapp[dy][dx]*=2;
+                    now=0;
+                    break;
+                }
+            }
+            //다음 항에 값 없으면 옮기기
+            mapp[dy][dx]=now;
+            now=0;
+        }
+        
+    }
+    
+    int m=5;
+    while(m--){
+        bool isTrue=false;
+        int cnt=(int) pv.size(), ans=0;
+        while(cnt--){
+            
+        }
+        if(isTrue){
+            maxx=max(maxx, ans);
+            break;
+        }
+    }
+     
+}
+*/
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin>>n;
+    //5번 이동하여 얻을 수 있는 점수의 최대값 구하기.
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>mapp[i][j];
+            if(mapp[i][j]!=0) pv.push_back(make_pair(i,j));
+        }
+    }
+    solve(0,0);
+    
+    /*
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<mapp[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+     */
+    
     return 0;
 }
+
+/*
+ 2048게임 구현.
+ 최대 5번 이동시켜서 얻을 수 잇는 가장 큰 블록을 출력.
+ */
