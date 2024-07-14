1e80467a43be433164549bbed21a5652c659e11e
YerangPark <smsms_@naver.com>
2024-07-14 10:12:23 +0900

    Solve Problem from Programmers

---
+++ Programmers/Lv1/카드뭉치.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "Yes";
+    int idx1 = 0, idx2 = 0;
+    for (int i=0; i<goal.size(); i++) {
+        if (goal[i] == cards1[idx1]) {
+            idx1++;
+        }
+        else if (goal[i] == cards2[idx2]) {
+            idx2++;
+        }
+        else {
+            answer = "No";
+            break;
+        }
+    }
+    return answer;
+}
