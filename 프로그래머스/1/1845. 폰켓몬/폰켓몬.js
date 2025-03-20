14607a5ff037f27feb08b04e2fe403b9409c4aa5
박예랑 | Yerang Park <buuuuung@naver.com>
2025-03-20 17:36:51 +0900

    [level 1] Title: 폰켓몬, Time: 1.05 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,10 @@
+function solution(nums) {
+    var answer = 0;
+    let set = new Set();
+    const N = nums.length;
+    for (let i=0; i<N; i++) {
+        set.add(nums[i]);
+    }
+    answer = set.size > N/2 ? N/2 : set.size;
+    return answer;
+}
